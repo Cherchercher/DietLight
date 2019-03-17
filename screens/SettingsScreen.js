69c752880fa4e9257c3eb6341e674bb46e8cb854
Jake <jake.fulton5@gmail.com>
2019-03-17 15:43:51 -0700

    Analytics page

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { ProgressCircle } from 'react-native-svg-charts'
-
-
+import { ProgressCircle, LineChart, YAxis, Grid } from 'react-native-svg-charts'
 
 
 const styles = StyleSheet.create({
@@ -14,7 +12,19 @@ const styles = StyleSheet.create({
   },
   text:{
     marginLeft:'auto',
-    marginRight:'auto',
+    marginRight: 15,
+  },
+  ptext:{
+    marginLeft:'auto',
+    marginRight: 'auto',
+  },
+  chart: {
+      marginLeft: 16,
+  },
+  row: {
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      flexDirection: "row",
   }
 });
 
@@ -28,8 +38,12 @@ class SettingsScreen extends React.Component {
       this.state = {
             total:2000,
             accu:1500,
-            isLoading: true
-      
+            burned: 250,
+            isLoading: true,
+            data1 : [55, 300, 90, 60, 56, 69, 91],
+            data2 : [91, 69, 56, 60, 90, 62, 330],
+            data3 : [82, 153, 63,  100, 113, 94, 116],
+            contentInset : {top: 20, bottom: 20}
       } 
     }
 
@@ -42,22 +56,98 @@ class SettingsScreen extends React.Component {
 
   render() {
   
-  const { total, accu } = this.state;
+  const { total, accu, burned, contentInset, data1, data2, data3 } = this.state;
   return (
     <View>
-      <ProgressCircle
-          style={ { height: 200 } }
-          progress={ 0.75 }
-          progressColor={'rgb(134, 65, 244)'}
-          startAngle={ -Math.PI * 0.8 }
-          endAngle={ Math.PI * 0.8 }
-      />
-      <Text style={styles.text}> Calories Acumulated: {accu}/{total}</Text>
+        <View>
+            <ProgressCircle
+                  style={ { height: 200 } }
+                  progress={ 0.75 }
+                  progressColor={'rgb(134, 65, 244)'}
+                  startAngle={ 0 }
+                  endAngle={ Math.PI * 2 }
+                  strokeWidth = { 35 }
+            />
+            <Text style={styles.ptext}> Calories Acumulated: {accu}/{total}</Text>
+            <Text style={styles.ptext}> Calories Burned: {burned}</Text>
+        </View>
+        <View style={{ height: 200, flexDirection: 'row' }}>
+                    <YAxis
+                        data={ data1 }
+                        contentInset={ contentInset }
+                        svg={{
+                            fill: 'grey',
+                            fontSize: 10,
+                        }}
+                        numberOfTicks={ 10 }
+                        formatLabel={ value => `${value}` }
+                        min={0}
+                        max={400}
+                    />
+                    <LineChart
+                        style={{ flex: 1, marginLeft: 16 }}
+                        data={ data1 }
+                        svg={{ stroke: 'rgb(134, 65, 244)' }}
+                        contentInset={ contentInset }
+                        yMin={0}
+                        yMax={400}
+                    >
+                        <Grid/>
+                    </LineChart>
+                    <LineChart
+                        //style={{ flex: 1, height: 200, marginLeft: 16 }}
+                        style={[StyleSheet.absoluteFill, styles.chart]}
+                        data={ data2 }
+                        svg={{ stroke: 'rgb(200, 0, 180)' }}
+                        contentInset={ contentInset }
+                        yMin={0}
+                        yMax={400}
+                    >
+                        <Grid/>
+                    </LineChart>
+                    <LineChart
+                        //style={{ flex: 1, height: 200, marginLeft: 16 }}
+                        style={[StyleSheet.absoluteFill, styles.chart]}
+                        data={ data3 }
+                        svg={{ stroke: 'rgb(0, 300, 100)' }}
+                        contentInset={ contentInset }
+                        yMin={0}
+                        yMax={400}
+                    >
+                        <Grid/>
+                    </LineChart>
+        </View>
+        <View style={styles.row}>
+            <Text style={styles.text}> Breakfast: </Text>
+            <View style = {{       width: 10,
+                                   height: 10,
+                                   backgroundColor: 'green',
+                                   marginTop: 5
+                          }}         
+            />
+        </View>
+        <View style={styles.row}>
+            <Text style={styles.text}> Lunch: </Text>
+            <View style = {{       width: 10,
+                                   height: 10,
+                                   backgroundColor: 'red',
+                                   marginTop: 5
+                          }}         
+            />
+        </View>
+        <View style={styles.row}>
+            <Text style={styles.text}> Dinner: </Text>
+            <View style = {{       width: 10,
+                                   height: 10,
+                                   backgroundColor: 'purple',
+                                   marginTop: 5
+                          }}         
+            />
+        </View>
     </View>
-)
+ )
 }
 }
-
 
 export default SettingsScreen;
 
