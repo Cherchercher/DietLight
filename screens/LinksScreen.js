af9c2d96cb0eaa2be2c24ee7717c514bad4a4997
Jake <jake.fulton5@gmail.com>
2019-03-20 19:58:00 -0700

    Picker for LinkScreen

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,36 +1,51 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { ScrollView, StyleSheet, View, Text, Picker } from 'react-native';
 import Parse from '../APIs/Parse';
 
 
 export default class LinksScreen extends React.Component {
-  constructor(props) {
-    super(props);
-      this.state = {
-                Parse:new Parse(),
-                isLoading: true,
-      }
- }
+    static navigationOptions = ({ navigation }) => ({
+        title: `Manual Entry`,
+    });
+    state = {time: ''}
+    updateTime = (time) => {
+        this.setState({ time: time })
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            Parse:new Parse(),
+            isLoading: true,
+        }
+    }
 
-componentDidMount() {
-    this.state.Parse.create('MedicalRecord',{'maxFat':44,'maxCalorie':2000,'userId':1})
-}
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
-    );
-  }
+    render() {
+        return (
+            <View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                    <Picker 
+                        selectedValue = {this.state.time}
+                        onValueChange = {this.updateTime}
+                        style={{height: 50, width: 150}}
+                        >
+                        <Picker.Item label = "Breakfast" value = "breakfast" />
+                        <Picker.Item label = "Lunch" value = "lunch" />
+                        <Picker.Item label = "Dinner" value = "dinner" />
+                    </Picker>
+                    <Text style={styles.text}> {this.state.time} </Text>
+                    <Text style={styles.text}> BSUG INPUT </Text>
+                </View>
+            </View>
+            
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+  text: {
+    fontSize: 20,
+    marginTop: 50,
+    alignSelf: 'center',
+    color: 'red'
   },
 });
